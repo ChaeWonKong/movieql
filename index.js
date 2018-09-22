77b62002b4f0ebd0ab38ab8bcf6ed01891c133d9
공채원 <chaewonkong@gmail.com>
2018-09-22 18:26:23 +0900

    create schema

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
 
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql"
+});
 
 server.start(() => console.log("GraphQL server Running"));
